601e48176e5b2088897ec1ba6a333aa07679ac3c
Gleb Ivanov <g.ivanov@fun-box.ru>
2018-03-02 13:24:26 +0400

    fixed rest args bug | bumped version

--- src/ts/dependencies/dependency.resolver.ts
+++ src/ts/dependencies/dependency.resolver.ts
@@ -2,7 +2,7 @@ import { Container } from '@/container';
 import { Dependency } from './dependency';
 
 export class DependencyResolver {
-    private extraArgsCounter: number = 0;
+    private extraArgsIndex: number = 0;
 
     public constructor(
         private container: Container,
@@ -11,19 +11,20 @@ export class DependencyResolver {
 
     public combineDepsWith(extraArgs: Array<any> = []): Array<any> {
         const resolvedDeps = this.combineDeps(extraArgs);
-        this.extraArgsCounter = 0;
-        return resolvedDeps;
+        const restArgs = extraArgs.slice(this.extraArgsIndex, extraArgs.length);
+        this.extraArgsIndex = 0;
+        return resolvedDeps.concat(restArgs);
     }
 
     private combineDeps(extraArgs: Array<any>): Array<any> {
-        return this.dependencies.map((dependency: Dependency<any>) =>
-            this.resolve(dependency, extraArgs)
+        return this.dependencies.map(
+            (dependency: Dependency<any>) => this.resolve(dependency, extraArgs)
         );
     }
 
     private resolve(dependency: Dependency<any>, extraArgs: Array<any>): any {
         return dependency.isInjectable() ?
             this.container.resolve(dependency.getClass()) :
-            extraArgs[this.extraArgsCounter++];
+            extraArgs[this.extraArgsIndex++];
     }
 }
